de71383f017b18e5f15917ba0012e7c6712ec645
EdenAlmighty <edengilady1@gmail.com>
2024-02-26 19:48:26 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -6,22 +6,58 @@ let gCurrShape = ''
 let gPen = { pos: null, isDown: false }
 let gLine = []
 
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
+
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+    // const touchArea = document.querySelector('.touch')
+
+    addListeners()
+    resizeCanvas()
+
+}
+function addListeners() {
+    addTouchListeners()
+    addMouseListeners()
+
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        //Calc the center of the canvas
+        const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+    })
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('click', onDraw)
+    gElCanvas.addEventListener('mousedown', onStartLine)
+    gElCanvas.addEventListener('mousemove', onDrawLine)
+    gElCanvas.addEventListener('mouseup', onEndLine)
+}
+
+
+function addTouchListeners() {
+
+    gElCanvas.addEventListener('touchstart', onStartLine)
+    gElCanvas.addEventListener('touchmove', onDrawLine)
+    gElCanvas.addEventListener('touchend', onEndLine)
 }
 
 
 function onStartLine(ev) {
     gPen.pos = { x: ev.offsetX, y: ev.offsetY }
     gPen.isDown = true
-    // gCtx.strokeStyle = 'green'
 
     gLine = []
     gLine.push(gPen.pos)
-    // console.log(gCtx);
-    // console.log(gLine);
-    // gCtx.drawArc()
+
     console.log(gPen);
     gCtx.beginPath()
     gCtx.moveTo(gPen.pos.x, gPen.pos.y)
@@ -29,16 +65,18 @@ function onStartLine(ev) {
 
 function onDrawLine(ev) {
     if (!gPen.isDown) return
-    const { offsetX, offsetY } = ev
 
+    gPen.pos = getEvPos(ev)
+    gCtx.beginPath()
+    // gCtx.lineWidth = 4
+    // if (!onDraw(ev)) gCtx.stroke()
+    // if (!onDraw(ev)) gCurrShape = 'pen'
     onDraw(ev)
-    gPen.pos = { x: offsetX, y: offsetY }
-    gLine.push(gPen.pos)
-
+    // console.log(gCurrShape);
+    console.log(gPen.pos.x, gPen.pos.y);
+    gLine.push(gPen.pos.x, gPen.pos.y)
+    
     // gLine.
-
-    gCtx.lineTo(offsetX, offsetY)
-    gCtx.stroke()
 }
 
 function onEndLine(ev) {
@@ -46,27 +84,33 @@ function onEndLine(ev) {
     gCtx.closePath()
 }
 
+function drawPen(x, y){
+    
+    gCtx.beginPath()
+    
+    gCtx.lineWidth = 4
+    
+    gCtx.stroke()
+
+    gCtx.lineTo(gPen.pos.x, gPen.pos.y)
+}
+
 function drawRect(x, y) {
     gCtx.beginPath()
 
     gCtx.lineWidth = 4
-    gCtx.strokeRect(x, y, 120, 120)
+    gCtx.strokeRect(x - 60, y - 60, 120, 120)
 
-    gCtx.fillRect(x, y, 120, 120)
+    gCtx.fillRect(x - 60, y - 60, 120, 120)
 }
 
 function drawArc(x, y) {
     gCtx.beginPath()
 
-    // The x,y cords of the center, radius,
-    // starting angle & ending angle, in radians
-
-    gCtx.arc(x, y, 70, 0, 2 * Math.PI) // draws a circle
+    gCtx.arc(x, y, 70, 0, 2 * Math.PI)
 
     gCtx.lineWidth = 4
     gCtx.stroke()
-
-    // gCtx.fill()
 }
 
 function onSetShape(shape) {
@@ -88,20 +132,71 @@ function onClearCanvas() {
 }
 
 function onDraw(ev) {
-    const { offsetX, offsetY } = ev
+    // const { offsetX, offsetY } = ev
+    
+    console.log(gCurrShape);
+    //In touch event were not using the offset. instead we need to use the func getEvPos!
+    const pos = getEvPos(ev)
 
     switch (gCurrShape) {
-        case 'triangle':
-            drawTriangle(offsetX, offsetY)
+        case 'pen':
+            drawPen(pos.x, pos.y)
             break
         case 'rect':
-            drawRect(offsetX, offsetY)
+            drawRect(pos.x, pos.y)
             break
         case 'circle':
-            drawArc(offsetX, offsetY)
+            drawArc(pos.x, pos.y)
             break
         case 'line':
-            drawLine(offsetX, offsetY)
+            drawLine(pos.x, pos.y)
             break
     }
+}
+
+
+function getEvPos(ev) {
+    let pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    }
+
+    if (TOUCH_EVENTS.includes(ev.type)) {
+
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        // Calc pos according to the touch screen
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        }
+    }
+    return pos
+}
+
+
+// DOWNLOAD CANVAS
+function downloadImg() {
+    const imgContent = gElCanvas.toDataUrl()
+    elLink.href = imgContent
+}
+
+// UPLOAD TO CANVAS
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+// Read the file from the input
+// When done send the image to the callback function
+
+function loadImageFromInput(ev, onImageReady) {
+    const reader = new FileReader()
+
+    reader.onload = ev => {
+        let img = new Image()
+        img.src = ev.target.result
+        img.onload = () => onImageReady(img)
+    }
+    reader.readAsDataURL(ev.target.files[0])
+
 }
